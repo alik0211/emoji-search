0c06d1c4b469908520188365ad48762f7ced0d79
alik0211 <alik0211alik@gmail.com>
2017-08-01 21:01:56 +0400

    :pencil: Using destructurization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import SearchInput from './SearchInput';
 import EmojiResults from './EmojiResults';
 import filterEmoji from './filterEmoji';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
