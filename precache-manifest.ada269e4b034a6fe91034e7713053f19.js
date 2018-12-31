509723205a7c583f49c5fdb817c0ab3b02406c8a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 02:45:53 +0000

    Deploy alik0211/emoji-search to github.com/alik0211/emoji-search.git:gh-pages

---
+++ precache-manifest.ada269e4b034a6fe91034e7713053f19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fcb811e8042ee44a631",
+    "url": "/emoji-search/static/js/runtime~main.5fcb811e.js"
+  },
+  {
+    "revision": "fe9f58109a5e7925b1d0",
+    "url": "/emoji-search/static/js/main.fe9f5810.chunk.js"
+  },
+  {
+    "revision": "04cd8e06ad2b22882616",
+    "url": "/emoji-search/static/js/1.04cd8e06.chunk.js"
+  },
+  {
+    "revision": "fe9f58109a5e7925b1d0",
+    "url": "/emoji-search/static/css/main.f4c2ff58.chunk.css"
+  },
+  {
+    "revision": "716db750fa24fcf2f480b20625c48d6e",
+    "url": "/emoji-search/index.html"
+  }
+];
